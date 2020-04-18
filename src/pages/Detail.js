f446e577f36ed2e5e127d8f72c17445416bb4d38
Ken Stone <ken@stoneblue.com>
2020-04-18 10:42:47 -0500

    from country

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -6,7 +6,10 @@ class Detail extends React.Component {
     render() {
         return( 
           <div>
-          <p>Hello, {chance.first()}!</p>
+          <p>Hello, {chance.first()} from {chance.country({ full: true })}!</p>
+          <p>Hello, {chance.first()} from {chance.country({ full: true })}!</p>
+          <p>Hello, {chance.first()} from {chance.country({ full: true })}!</p>
+          <p>Hello, {chance.first()} from {chance.country({ full: true })}!</p>
           <p>{this.props.message}</p>
           </div>
         );
