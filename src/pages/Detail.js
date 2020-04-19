f041b16a7b465f8dd1ceec123d3d89eaeec8a9bf
Ken Stone <ken@stoneblue.com>
2020-04-19 16:27:01 -0500

    jsx onClick event

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -3,14 +3,23 @@ import Chance from 'chance';
 
 
 class Detail extends React.Component {
+    buttonClicked() {
+        console.log('Button was clicked!')
+        this.forceUpdate();
+    }
+
     render() {
         return( 
           <div>
           <p>Hello, {chance.first()} from {chance.country({ full: true })}!</p>
+
           <p>Hello, {chance.first()} from {chance.country({ full: true })}!</p>
+
+
           <p>Hello, {chance.first()} from {chance.country({ full: true })}!</p>
           <p>Hello, {chance.first()} from {chance.country({ full: true })}!</p>
           <p>{this.props.message}</p>
+          <button onClick={this.buttonClicked.bind(this)}>Meet Someone New</button>
           </div>
         );
     }
