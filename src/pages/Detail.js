b9c967ddf398b7c9e79987dbeeb67fd6db9badda
Ken Stone <ken@stoneblue.com>
2020-04-19 19:35:24 -0500

    setState

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -3,15 +3,40 @@ import Chance from 'chance';
 
 
 class Detail extends React.Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            name: chance.first(),
+            country: chance.country({ full: true })
+        };
+    }
+
+    shouldComponentUpdate(){
+      return true;
+    }
+
+
+    shouldUpdateComponent(){
+      return false;
+    }
+
     buttonClicked() {
         console.log('Button was clicked!')
-        this.forceUpdate();
+        const newState = {
+            name: chance.first()
+        };
+    
+        this.setState(newState);
+
+        //this.forceUpdate();
     }
 
     render() {
         return( 
           <div>
-          <p>Hello, {chance.first()} from {chance.country({ full: true })}!</p>
+          <p>Hello, {this.state.name} from {this.state.country}!</p>
 
           <p>Hello, {chance.first()} from {chance.country({ full: true })}!</p>
 
